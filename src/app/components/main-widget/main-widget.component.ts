85a29bdbb037e97af09cd658f21c5a259935137b
Gosia Zaremba <gosia@szel.net>
2024-04-19 13:23:34 +0200

    Added hourly weather service and bootstrap.

--- src/app/components/main-widget/main-widget.component.ts
+++ src/app/components/main-widget/main-widget.component.ts
@@ -1,6 +1,7 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CurrentWeatherService } from '../../services/current-weather.service';
 import { DailyWeatherService } from '../../services/daily-weather.service';
+import { HourlyWeatherService } from '../../services/hourly-weather.service';
 
 @Component({
   selector: 'app-main-widget',
@@ -10,19 +11,17 @@ import { DailyWeatherService } from '../../services/daily-weather.service';
   styleUrl: './main-widget.component.scss',
 })
 export class MainWidgetComponent {
-  currentDataService: any = inject(CurrentWeatherService);
-  dailyDataService: any = inject(DailyWeatherService);
+  currentDataService = inject(CurrentWeatherService);
+  dailyDataService = inject(DailyWeatherService);
+  hourlyDataService = inject(HourlyWeatherService);
 
   latitude = '54.21';
   longitude = '18.38';
-  days = 3;
-
-  constructor() {
-    effect(() => console.log(this.dailyDataService.getDailyWeatherData()));
-  }
+  days = '3';
 
   ngOnInit() {
     this.currentDataService.fetchData(this.latitude, this.longitude);
     this.dailyDataService.fetchData(this.latitude, this.longitude, this.days);
+    this.hourlyDataService.fetchData(this.latitude, this.longitude, this.days);
   }
 }
